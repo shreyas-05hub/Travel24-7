0b48f295626e9f031ec4de99e05017fb76c3e4cd
shreyas-05hub <shreyaskandekar07@gmail.com>
2025-10-16 16:11:10 +0530

    setup

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,16 +3,22 @@ import Home from './pages/Home'
 import Packages from './pages/Packages'
 import Recommendations from './pages/Recommendations'
 import Destinations from './pages/Destinations'
+import { Routes,Route } from 'react-router-dom'
+import Navbar from './components/layout/Navbar'
+import Footer from './components/layout/Footer'
 
 const App = () => {
   return (
     <div>
-      <Routes>
+      <Navbar>
+          <Routes>
               <Route path="/" element={<Home />} />
               <Route path="/packages" element={<Packages />} />
               <Route path="/recommendations" element={<Recommendations />} />
               <Route path="/destinations" element={<Destinations />} />
-            </Routes>
+          </Routes>
+      </Navbar>
+      <Footer/>
     </div>
   )
 }
--- frontend/src/components/layout/Navbar.jsx
+++ frontend/src/components/layout/Navbar.jsx
@@ -1,11 +1,51 @@
 import React from 'react'
+import { NavLink } from 'react-router-dom';
 
 const Navbar = () => {
   return (
-    <div>
-      
-    </div>
+    <nav className="navbar navbar-expand-lg navbar-light bg-light"> 
+      <div className="container-fluid">
+        {/* Bootstrap brand/logo class */}
+        <NavLink to="/" className="navbar-brand">
+          TravelSite 
+        </NavLink>
+
+        {/* Bootstrap toggler button for smaller screens */}
+        <button 
+          className="navbar-toggler" 
+          type="button" 
+          data-bs-toggle="collapse" 
+          data-bs-target="#navbarNav" 
+          aria-controls="navbarNav" 
+          aria-expanded="false" 
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
+
+        {/* Collapsible content for the navigation links */}
+        <div className="collapse navbar-collapse" id="navbarNav">
+          {/* Bootstrap class for unordered list of links */}
+          <ul className="navbar-nav mx-auto mb-2 mb-lg-0 "> 
+            {/* Nav Item (li) and Nav Link (NavLink) classes */}
+            <li className="nav-item">
+              <NavLink to="/" className="nav-link" activeclassname="active">Home</NavLink>
+            </li>
+            <li className="nav-item">
+              <NavLink to="/packages" className="nav-link" activeclassname="active">Packages</NavLink>
+            </li>
+            <li className="nav-item">
+              <NavLink to="/recommendations" className="nav-link" activeclassname="active">AI Recommendations</NavLink>
+            </li>
+            <li className="nav-item">
+              <NavLink to="/destinations" className="nav-link" activeclassname="active">Destinations</NavLink>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+
   )
 }
 
-export default Navbar
+export default Navbar;
